e4b9979d432af8f9fedb811b4ff3659dcf1bd468
Reinaldo Contreras <reinaldojcg1@gmail.com>
2024-05-03 14:42:15 -0500

    Remove header

--- src/presentation/navigator/Navigator.tsx
+++ src/presentation/navigator/Navigator.tsx
@@ -5,7 +5,10 @@ const Stack = createStackNavigator();
 
 export const Navigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
